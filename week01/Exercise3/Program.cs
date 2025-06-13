6c1cd5bbb9eb7e156ff919a7bf76a1eaf9a1293d
NathanFenton <ntfenton5@gmail.com>
2025-06-12 22:05:25 -0600

    Finished Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string wdyt = Console.ReadLine();
+            guess = int.Parse(wdyt);
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        } while (guess != number);
+
+        Console.WriteLine("You guessed it!");
+
     }
 }
