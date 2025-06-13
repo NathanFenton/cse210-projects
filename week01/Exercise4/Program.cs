d0b327d87918b8aba2c972b1d9f7f78e08d2b879
NathanFenton <ntfenton5@gmail.com>
2025-06-12 23:01:30 -0600

    Finished Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string num = Console.ReadLine();
+            number = int.Parse(num);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            
+        }
+
+        int sum = 0;
+
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+
+        int numOfNumbers = numbers.Count;
+
+        float average = sum / numOfNumbers;
+
+        int largestNum = numbers.Max();
+        //I looked up a built in method to find the largest number and found the .Max()
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largestNum}");
+
     }
 }
