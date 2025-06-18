fe268333c00b8606e4a76860a9947bf88266d850
NathanFenton <ntfenton5@gmail.com>
2025-06-18 11:14:45 -0600

    Finished Resumes Program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,25 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Lead IT";
+        job1._startYear = 2000;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "Space X";
+        job2._jobTitle = "Janator";
+        job2._startYear = 1999;
+        job2._endYear = 2019;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Random Person";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.DisplayResumeDetails();
+
     }
 }
